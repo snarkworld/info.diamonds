8ceb40fc40024e458222a07bf4384b889758ed24
gwei.world <john@gwei.world>
2022-10-18 19:41:05 -0600

    theme config update

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   titleSuffix: ' – info.diamonds',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">info.diamonds</span>
+      <span className="mr-2 font-extrabold hidden md:inline">💎 info.diamonds</span>
       <span className="text-gray-600 font-normal hidden md:inline">
       </span>
     </>
