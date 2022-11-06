658d6fb35d404916edf2d94857fd349aba7aa65b
gwei.world <john@gwei.world>
2022-11-06 14:39:06 -0700

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import 'nextra-theme-docs/style.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+  <>
+  <Component {...pageProps} />
+  <Analytics />
+  </>
+
 }
